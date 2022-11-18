9fad69a5bbcab496315ab851776832a4a8e8c982
juno <116588160+Brusia23@users.noreply.github.com>
2022-11-18 20:50:47 +0000

    I followed labs 1, 2, and 3. I changed the replit file to launch the Conversation.java and followed the instructions in the lab, such as creating new user inputs and printing the results in the console. I also updated Computer.java and WeirdOps.java.

--- Conversation.java
+++ Conversation.java
@@ -11,6 +11,8 @@ public class Conversation {
 	int birthyear;
 	double priceOrange;
 	double pricePeanuts;
+      double priceWatermelon;
+      double priceBread;
 	double totalPrice=0;  //initialize to 0
    
     System.out.println("Welcome. Type your first name and press enter.");
@@ -29,12 +31,27 @@ public class Conversation {
 	System.out.println("Approximate age: " + (2022-birthyear));
    
 	//Part 5 code goes here.
+    int houseNumber;
+  	String streetName;
+    String cityName;
+    String stateName;
+    int zipCode;
+  	System.out.print("Enter house number> ");
+  	houseNumber=reader.nextInt();
+    reader.nextLine();
+  	System.out.print("Enter street name> ");
+  	streetName = reader.nextLine();
 
    
 
     
     //Part 6 Add code to read the rest of the address (city, state, zipcode) and print it
-
+    System.out.print("Enter city name> ");
+  	cityName = reader.nextLine();
+    System.out.print("Enter state name> ");
+  	stateName = reader.nextLine();
+    System.out.print("Enter zip code> ");
+  	zipCode = reader.nextInt();
 
 
     //////////////////////////////
@@ -46,10 +63,19 @@ public class Conversation {
 	pricePeanuts = reader.nextDouble();
 	totalPrice += pricePeanuts;
     //Part 4 code for bread and watermelon
+      System.out.print("How much does bread cost? $");
+      priceBread = reader.nextDouble();
+      totalPrice += priceBread;
+      System.out.print("How much does watermelon cost? $");
+      priceWatermelon = reader.nextDouble();
+      totalPrice += priceWatermelon;
 
 
-
-	System.out.println("The total cost is $" + totalPrice);
+    System.out.println("Hi, " + firstName + " " + middleInitial + ". " + lastName + ".");
+    System.out.println("You were born in " + birthyear + ", making you " + (2022-birthyear) + " years old!");
+    System.out.println("You live in " + houseNumber + " " + streetName + " in " + cityName + ", " + stateName + ", with the zip code " + zipCode + ".");
+	System.out.println("The total cost of your items is $" + totalPrice);
+      
    
 
 
